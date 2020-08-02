10c8597b0fbd4a71b3a2d599586f2e1cca59b767
Parsa KamaliPour <parsakamalipour@gmail.com>
2020-08-02 20:41:02 +0430

    done

--- Assets/LevelController.cs
+++ Assets/LevelController.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static int _nextLevelIndex = 1;
+    private Enemy[] _enemies;
+
+    private void OnEnable()
     {
-        
+        _enemies = FindObjectsOfType<Enemy>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        foreach(Enemy enemy in _enemies)
+        {
+            if (enemy != null)
+                return;
+
+        }
+        Debug.Log("You killed all the enemies");
+        _nextLevelIndex++;
+        string nextLevelName = "Level" + _nextLevelIndex;
+        SceneManager.LoadScene(nextLevelName);
     }
 }
